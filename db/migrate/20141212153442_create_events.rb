2b63e0dc773d17bc6ca7d429185b58697e69010a
Brian Borge <brianborge@gmail.com>
2014-12-12 17:20:56 -0500

    created migrations user, event and user_event tables

--- db/migrate/20141212153442_create_events.rb
+++ db/migrate/20141212153442_create_events.rb
@@ -1,4 +1,14 @@
 class CreateEvents < ActiveRecord::Migration
   def change
+    create_table :events do |t|
+      t.date :exchange_date
+      t.date :draw_date
+      t.integer :max_price
+      t.string :location
+      t.references :user #admin
+      t.string :invite_key
+
+      t.timestamps
+    end
   end
 end
