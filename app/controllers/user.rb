e922e277eb9ec3cb8259204f4b7493f85ce70ea8
Brian Borge & Luke Thomas <pair+admiralrico.lukert33@devbootcamp.com>
2014-12-13 12:06:35 -0500

    created landing page with user creation

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,24 +1,23 @@
-get '/users' do
-  # display a list of all users
-  # @users = User.all
-  # erb :'users/index'
-end
+# get '/users' do
+#   # display a list of all users
+#   # @users = User.all
+#   # erb :'users/index'
+# end
 
 get '/users/new' do
   # return an HTML form for creating a new user
-  # erb :'users/new'
+  erb :'users/new'
 end
 
 post '/users' do
-  # create a new user
-  # @user = User.create(params[:user])
-  # redirect '/users'
+  @user = User.create(params[:user])
+  redirect "/users/#{@user.id}"
 end
 
 get '/users/:id' do |id|
-  # display a specific user
-  # @user = User.find id
-  # erb :'users/single'
+  @user = User.find id
+  @events = @user.events
+  erb :'users/exchanges'
 end
 
 get '/users/:id/edit' do |id|
