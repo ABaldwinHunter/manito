91746fd3c7a1404a4fcc1990e7c09c131b4f7d3b
Ashley Baldwin-Hunter <ashley.baldwinhunter@gmail.com>
2014-12-14 15:12:51 -0500

    refactored event rb code and added signin validation

--- app/helpers/session_helpers.rb
+++ app/helpers/session_helpers.rb
@@ -1,3 +1,21 @@
 def current_user
-  @user ||= User.find session[:user_id]
+  @user ||= User.find_by_id session[:user_id]
+end
+
+def signed_in!
+  redirect '/' unless user_signed_in?
+end
+
+def user_signed_in?
+  !!session[:user_id]
+end
+
+def authorize_event! event_id
+  @event = Event.find event_id
+end
+
+def authorize_owner!(object, &block)
+  if user_signed_in? && current_user.id == object.admin_id
+    block.call!
+  end
 end
