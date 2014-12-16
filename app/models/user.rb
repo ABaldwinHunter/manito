396de13577a2b517bb31bc9b732eef38585d817c
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2014-12-15 22:06:17 -0500

    fixed sender

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
 
   def send_email_invites(email_addresses, sender, draw_date, admin, key, max)
     email_addresses.each do |email_address|
-      Pony.mail(:to => email_address,# :from => sender,
+      Pony.mail(:to => email_address,
+                :from => sender,
                 :subject => 'Secret Santa Invite',
                 :html_body => "Congratulations! You have been invited to participate in Manito's Secret Santa exchange, hosted by #{admin}! You may register or login at <a href='https://thawing-fortress-5755.herokuapp.com'>Manito Secret Santa Exchange</a>, and find your event using the secret key: #{key}. Max price is $#{max}. Don't forget to sign up by #{draw_date} to participate!")  
     end
